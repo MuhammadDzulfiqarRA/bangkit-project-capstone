7707de592b3efcc2464bb935b550d53b77c6ef8b
Muhammad Dzulfiqar <128130237+MuhammadDzulfiqarRA@users.noreply.github.com>
2024-06-21 15:24:33 +0700

    Delete app directory

--- app/src/main/java/com/dicoding/abai/helper/ConstantsObject.kt
+++
@@ -1,7 +0,0 @@
-package com.dicoding.abai.helper
-
-class ConstantsObject {
-    companion object {
-        const val DASHBOARD_TO_DETAIL: String = "DASHBOARD_TO_DETAIL"
-    }
-}
--- app/src/main/java/com/dicoding/abai/viewmodel/LoginViewModel.kt
+++
@@ -1,4 +0,0 @@
-package com.dicoding.abai.viewmodel
-
-class LoginViewModel {
-}
